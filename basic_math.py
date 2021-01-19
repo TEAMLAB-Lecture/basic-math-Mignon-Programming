1c7a98d8f7b7b3c0c7e20267e268f5cc22aa38b8
Kang Min Yong <ricky23@yonsei.ac.kr>
2021-01-19 15:03:27 +0900

    Update My code

--- basic_math.py
+++ basic_math.py
@@ -78,7 +78,7 @@ def get_mean(number_list):
     for element in number_list:
         sum += element
 
-    mean = int(sum / len(number_list))
+    mean = sum / len(number_list)
     return mean
 
 
